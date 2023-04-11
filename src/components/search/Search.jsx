c1a4fe04e58a2433ec1d62b456976c4ab32b3fdd
Anurag Biswal <anurag.biswal0702@gmail.com>
2023-04-11 12:47:21 +0530

    Modified trends and suggestions

--- src/components/search/Search.jsx
+++ src/components/search/Search.jsx
@@ -8,18 +8,20 @@ const Search = () => {
 
   const searchEncoder = () => {
     const trends = [
-      faker.commerce.productName(),
-      faker.commerce.productName(),
-      faker.commerce.productName(),
-      faker.image.fashion(),
+      [
+        faker.commerce.productName(),
+        faker.commerce.productName(),
+        faker.commerce.productName(),
+        faker.commerce.productName(),
+      ],
+      [
+        faker.image.fashion("320", "240", true),
+        faker.image.fashion("320", "240", true),
+      ],
     ];
     setSuggestions(trends);
     setShowSuggestions(true);
   };
-
-  // const searchEncoder = (e) => {
-
-  // }
 
   return (
     <div className="search__container">
@@ -30,16 +32,41 @@ const Search = () => {
             <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
           </g>
         </svg>
-        <input placeholder="Search" type="search" class="input" onChange={searchEncoder} style={{ display: showSuggestions ? "block" : "none" }}/>
-      <button>Search</button>
+        <input
+          placeholder="Search"
+          type="search"
+          class="input"
+          onChange={searchEncoder}
+          style={{ display: showSuggestions ? "block" : "none" }}
+        />
+        <button>Search</button>
       </div>
       {/* <button onClick={handleClick}>Search</button> */}
-      {suggestions.length > 0 && (
+      {suggestions[0].length > 0 && (
         <div className="search__container-suggestion-box">
-          <h3>Latest Trend Suggestions:</h3>
+          <h3>Latest Trends:</h3>
           <ul>
             {suggestions.map((suggestion, index) => (
-              <li key={index}>{suggestion}</li>
+              <>
+                <li key={index}>
+                  <img src={suggestion} alt="" />
+                </li>
+                <li key={index}>
+                  <img src={suggestion} alt="" />
+                </li>
+              </>
+            ))}
+          </ul>
+        </div>
+      )}
+      {suggestions[1].length > 0 && (
+        <div className="search__container-suggestion-box">
+          <h3>Popular Suggestions:</h3>
+          <ul>
+            {suggestions.map((suggestion, index) => (
+              <>
+                <li key={index}>{suggestion}</li>
+              </>
             ))}
           </ul>
         </div>
